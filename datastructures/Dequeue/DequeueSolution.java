80ba5290536c91a3d6482e53394174f549cd922c
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-03-03 18:43:25 -0300

    Dequeue v2

--- datastructures/Dequeue/DequeueSolution.java
+++ datastructures/Dequeue/DequeueSolution.java
@@ -1,27 +1,44 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class DequeueSolution {
     public static void main(String[] args) {
-        long init = System.currentTimeMillis();
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
         Deque<Integer> integers = new ArrayDeque<>();
-        for (int i = 0; i < n; i++) {
-            integers.addLast(scanner.nextInt());
-        }
-
+        HashMap<Integer, Integer> hashSet = new HashMap<>();
         int max = 0;
-        for (int i = 0; i < integers.size(); i++) {
-            if (i + m <= integers.size()) {
-                Set<Integer> integerSet = new HashSet<>(new ArrayList<>(integers).subList(i, i + m));
+        for (int i = 0; i < n; i++) {
+            int in = scanner.nextInt();
+            integers.addLast(in);
+            addOrUpdateCount(in, hashSet);
+            if (integers.size() == m) {
+                max = Math.max(hashSet.size(), max);
+                removeOrUpdateCount(integers.getFirst(), hashSet);
                 integers.removeFirst();
-                max = Math.max(integerSet.size(), max);
             }
         }
         System.out.println(max);
-        System.err.println(System.currentTimeMillis() - init);
+    }
+
+    private static void removeOrUpdateCount(Integer first, HashMap<Integer, Integer> hashSet) {
+        Integer count = hashSet.get(first);
+        if (count == 1) {
+            hashSet.remove(first);
+        } else {
+            count--;
+            hashSet.put(first, count);
+        }
+    }
+
+    private static void addOrUpdateCount(int in, HashMap<Integer, Integer> hashSet) {
+        if (hashSet.get(in) == null) {
+            hashSet.put(in, 1);
+        } else {
+            Integer count = hashSet.get(in);
+            count++;
+            hashSet.put(in, count);
+        }
     }
 }
